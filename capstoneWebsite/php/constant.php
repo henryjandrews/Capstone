42c49fcc056c693bc49bb9c32f8c15ca89a8f2b9
Unknown <vincena2@miamioh.edu>
2016-11-08 18:32:01 -0500

    folder with db

---
+++ capstoneWebsite/php/constant.php
@@ -0,0 +1,7 @@
+<?php
+// Database constants
+define('DB_SERVER', "localhost:8889");
+define('DB_USER', "root");
+define('DB_PASSWORD', "root");
+define('DB_DATABASE', "countries");
+define('DB_DRIVER', "mysql");
