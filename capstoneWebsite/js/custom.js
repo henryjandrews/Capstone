0b9dff0365cde6f5a942d6a729613a353c74f91f
Unknown <vincena2@miamioh.edu>
2016-11-08 18:14:06 -0500

    folder with bootstrap, css, html

---
+++ capstoneWebsite/js/custom.js
@@ -0,0 +1,18 @@
+
+/*this function loads the html includes 
+this function is called to display navigation.html pages
+which contains the html code for search bar and naviagation bar
+*/
+$(function(){
+    $(".navigation").load("navigation.html");
+});
+
+
+/*this function loads upvote, downvotae and report
+symbol on all pages */
+
+
+$(function(){
+$(".symbols").load("updownVoteFlag.html");
+
+});
