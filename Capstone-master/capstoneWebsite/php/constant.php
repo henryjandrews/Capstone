f8e7a463d3c141720f9f201b8bf97cc14611cac4
mosleylm <mosleylm@miamioh.edu>
2016-11-09 14:38:17 -0500

    Updated database and constant.php
    Changed question_Text query and changed database constant info

--- Capstone-master/capstoneWebsite/php/constant.php
+++ Capstone-master/capstoneWebsite/php/constant.php
@@ -1,7 +1,7 @@
 <?php
 // Database constants
-define('DB_SERVER', "localhost:8889");
-define('DB_USER', "root");
-define('DB_PASSWORD', "root");
-define('DB_DATABASE', "countries");
+define('DB_SERVER', "localhost:3307");
+define('DB_USER', "keybank");
+define('DB_PASSWORD', "cse449");
+define('DB_DATABASE', "keybank");
 define('DB_DRIVER', "mysql");
