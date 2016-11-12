ee2e1811bd08c93efb5f0f29781414521fab20fa
vincena2 <vincena2@miamioh.edu>
2016-11-12 14:50:31 -0500

    added stylesheet link

--- Capstone-master/capstoneWebsite/mainPage.php
+++ Capstone-master/capstoneWebsite/mainPage.php
@@ -13,6 +13,7 @@
     <title>keySight</title><!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/custom.css" rel="stylesheet" type="text/css">
+     <link href="css/styles.css" rel="stylesheet" type="text/css">
 
   
 
