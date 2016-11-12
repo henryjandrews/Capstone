619f503ac1d15affbae5b102e8dfb98eb1164242
mosleylm <mosleylm@miamioh.edu>
2016-11-12 15:00:33 -0500

    constant.php changes
    changed database connection information

--- Capstone-master/capstoneWebsite/php/constant.php
+++ Capstone-master/capstoneWebsite/php/constant.php
@@ -1,7 +1,7 @@
 <?php
 // Database constants
-define('DB_SERVER', "localhost:8889");
-define('DB_USER', "root");
-define('DB_PASSWORD', "root");
-define('DB_DATABASE', "keybank");
+define('DB_SERVER', "keybank.cwdefzhbyz8e.us-east-1.rds.amazonaws.com:3307");
+define('DB_USER', "keybank");
+define('DB_PASSWORD', "liamcse449");
+define('DB_DATABASE', "mydb");
 define('DB_DRIVER', "mysql");
