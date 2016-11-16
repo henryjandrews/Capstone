ccc9e8ce4d8c0ade30eb1e6facce93c4ea11de71
Henry Andrews <henry@Henrys-MacBook-Pro-2.local>
2016-11-16 12:02:24 -0500

    Added ability to dismiss submit alert, created safegaurd for submitting duplicate questions

--- Capstone-master/capstoneWebsite/js/auto-complete.js
+++ Capstone-master/capstoneWebsite/js/auto-complete.js
@@ -1,4 +1,4 @@
-var MIN_LENGTH = 2;
+var MIN_LENGTH = 1;
 
 $( document ).ready(function() {
 	$("#keyword").keyup(function() {
@@ -7,23 +7,30 @@ $( document ).ready(function() {
 
 			$.get( "php/auto-complete.php", { keyword: keyword } )
 			.done(function( data ) {
+				var in_list = false;
 				$('#results').html('');
-				var askQuestion = "Ask a new question";
-				$('#results').append('<div class="item">' + askQuestion + '</div>');
+				var askQuestion = "Ask Question: "+$("#keyword").val();
+				$('#results').append('<div class="item" style="color:red;">' + askQuestion + '</div>');
 				var results = jQuery.parseJSON(data);
 				$(results).each(function(key, value) {
-					$('#results').append('<div class="item">' + value + '</div>');
+					$('#results').append('<div class="item style="color:black;">' + value + '</div>');
+					if ($("#keyword").val() == value){
+						in_list = true;
+					}
 				});
 
 			    $('.item').click(function() {
 			    	var text = $(this).html();
 			    	var question = $('#keyword').val();
-			    	if (text == askQuestion){
+			    	if (text == askQuestion && in_list == false){
 			    		$.post( "php/ask-question.php", { keyword: keyword } )
 			    		.done(function() {
 			    		$('#keyword').val("");
-			    		$('.searchBar').append('<div class="alert alert-success"><strong>Your Question has been asked!</strong></div>');
+			    		$('.searchBar').append('<div class="alert alert-success fade in"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a><strong>Your Question has been asked!</strong></div>');
 			    		});
+			    	}
+			    	else if (in_list == true){
+			    		$('.searchBar').append('<div class="alert alert-warning fade in"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a><strong>Question already exists</strong></div>');
 			    	}
 			    });
 
