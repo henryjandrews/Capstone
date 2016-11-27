cdb5afa58c68a443b2783e1f8f11fc19b2c7b6e6
vincena2 <vincena2@miamioh.edu>
2016-11-27 13:15:40 -0500

    Add files via upload

--- Capstone-master/capstoneWebsite/answerQuestionEA.php
+++ Capstone-master/capstoneWebsite/answerQuestionEA.php
@@ -1,86 +1,85 @@
 
-
-<!-- THIS IS THE PAGE FOR SUBMITTING ANSWERS -->
+<!--THIS IS THE MAIN PAGE -->
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
 
-    <meta charset="utf-8">
+     <meta charset="utf-8">
     <meta content="IE=edge" http-equiv="X-UA-Compatible">
     <meta content="width=device-width, initial-scale=1" name="viewport">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>keySight</title><!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/custom.css" rel="stylesheet" type="text/css">
-      
-    <script src="js/bootstrap.min.js">
-    </script>
+     <link href="css/styles.css" rel="stylesheet" type="text/css">
 
-    
-  
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+      <script src="js/bootstrap.min.js"> </script>
 
-    
+
 </head>
 
+
 <body>
-
-<?php include("nav.html"); ?>
-
-  
+ <?php include("nav.html"); ?>
 
 <div class = "container">
     <div class = "row">
-        <div class = "answerQuestion">
+
+<div class = "col-xs-sm-md-lg-12">
+
+<div class = "ans">
+
+    <div class = "col-xs-2 flag">
+    <?php include("updownVoteFlag.html"); ?>
+</div>
+
+<div class = "space4answer">
+
+    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem debitis iure pariatur in ipsam dolorem nulla consectetur officia nisi, magnam soluta aspernatur veritatis ratione accusantium, rerum expedita enim? Nihil, voluptatum. </p>
+    </div>
+   
+</div>
 
 
 
-<!-- The question(user clicked) should be displayed here -->
-<div class = "question">
+
+
+
+
     </div>
 
-    <div class = "answers"> 
-    "This is where answer will go"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 </div>
-<hr>
+
+        </div>
+        </div>
 
 
-<!-- This is where user answer the questions -->
-<div class="form-group">
-  <label for="Answer">Add a new  and improved answer:</label>
-
-  <!-- Text area for enter the question -->
-  <textarea class="form-control" rows="10" id="Anaswer"></textarea>
-</div>
-<!-- End of form-group class -->
-
-
-<!-- Submitting the answer -->
-<div class =  "submitButton">
-     <input type="submit" class="btn btn-info" value="Your Answer">
-</div>
-<!-- End of submitting aanswer method -->
-
-
-</div> <!-- end of myQuestions -->
-</div> <!-- end of row -->
-</div> <!-- end of container -->
-
-    <!--=========================End of navigation bar ==========-->
-          <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <!-- End of serachbar -------->
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src=
     "https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js">
     </script> 
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-   
+     
+    <script src="js/bootstrap.min.js">
+    </script>
     <script src = "js/custom.js"></script>
-    <script src = "js/answerQuestionEA.js"></script>
    
+      <script src="js/auto-complete.js"></script>
 </body>
 </html>
