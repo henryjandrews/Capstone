2b7fdb245573d88e1a306b51122937e48a0329f2
Henry Andrews <henry@Henrys-MacBook-Pro-2.local>
2016-11-27 15:43:13 -0500

    Added up/down vote to answers

--- Capstone-master/capstoneWebsite/js/answerQuestionEA.js
+++ Capstone-master/capstoneWebsite/js/answerQuestionEA.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
  var question = decodeURI($.getURLParam("question"));
  var qID = decodeURI($.getURLParam("qID"));
- $('.question').append('<p>Question: '+question+' ID = '+qID+'</p>');
+ $('.question').append('<p>Question: '+question+'</p>');
  $.get( "php/get-answers.php", { qID:qID } )
  	.done(function( data ) {
 		var results = jQuery.parseJSON(data);
@@ -14,7 +14,25 @@ $( document ).ready(function() {
 				var obj = results[i];
 				$('.forAnswers').append(
 					'<div class = "ansView">'+
-						'<div class = "col-xs-2 flag"></div>'+
+						'<div class = "col-xs-2 flag">'+
+							'<div class = "forVoting">'+
+       							'<div class = "thumbsDown" AnswerID = '+obj["AnswerID"]+'>'+
+									 '<button type="button" class="btn btn-default" style="border-color:blue" >'+
+                            			'<span class="glyphicon glyphicon-hand-up" aria-hidden="true"></span>'+
+                        			 '</button>'+
+        						'</div>'+
+
+								'<div class = "blank"></div>'+
+         						'<div class = "displayScore">'+
+         						'<p>'+obj["Score"]+'</p>'+
+        						'</div>'+
+ 								'<div class = "thumbsDown" AnswerID = '+obj["AnswerID"]+'>'+
+       						    	'<button type="button" class="btn btn-default" style="border-color:red" >'+
+                            		'<span class="glyphicon glyphicon-hand-down" aria-hidden="true"></span>'+
+                        			'</button>'+
+        						'</div>'+
+							'</div>'+
+						'</div>'+
 						'<div class = "space4answer"><p>'+obj["Answer_text"]+'</p></div>'+
 					'</div>'+
 					'<div class = "blank"></div>'
