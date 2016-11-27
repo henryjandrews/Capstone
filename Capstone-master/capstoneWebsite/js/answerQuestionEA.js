6bfca89f7d178bedc9988e4719db046b60182845
Henry Andrews <henry@Henrys-MacBook-Pro-2.local>
2016-11-27 13:59:18 -0500

    Stashing changes to git

--- Capstone-master/capstoneWebsite/js/answerQuestionEA.js
+++ Capstone-master/capstoneWebsite/js/answerQuestionEA.js
@@ -1,14 +1,21 @@
+$( document ).ready(function() {
 var question = decodeURI($.getURLParam("question"));
 var id = decodeURI($.getURLParam("id"));
-$( document ).ready(function() {
  $('.question').append('<p>Question: '+question+' ID = '+id+'</p>');
  
  $.get( "php/get-answers.php", { id:id } )
  .done(function( data ) {
 	var results = jQuery.parseJSON(data);
-	$(results).each(function(key, value) {
-		$('#results').append('<div class=".answers" style="color:black;">' + value+'</div>');
-	});
+	if ( results.length == 0 ){
+		$('.row').append('<p>There are no answers for this question yet</p>');
+	}
+	else{
+		for(var i = 0; i < results.length; i++){
+			var obj = results[i];
+			console.log[obj];
+			$('.row').append('<div class = "ansView"><div class = "col-xs-2 flag"><?php include("updownVoteFlag.html"); ?></div><div class = "space4answer"><p>'+obj["Answer_text"]+'</p></div></div><div class = "blank"></div>');
+		}
+	}
  });
 
 });
