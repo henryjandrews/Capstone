6359ede6bdc84a9aca3fafe42181768d9e2a6746
vincena2 <vincena2@miamioh.edu>
2016-11-27 15:33:00 -0500

    Add files via upload
    minor changes to main page

--- Capstone-master/capstoneWebsite/mainPage.php
+++ Capstone-master/capstoneWebsite/mainPage.php
@@ -14,6 +14,8 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/custom.css" rel="stylesheet" type="text/css">
      <link href="css/styles.css" rel="stylesheet" type="text/css">
+     <script src="js/bootstrap.min.js">
+    </script>
 
   
 
@@ -142,8 +144,7 @@
     </script> 
     <!-- Include all compiled plugins (below), or include individual files as needed -->
      
-    <script src="js/bootstrap.min.js">
-    </script>
+    
     <script src = "js/custom.js"></script>
    
       <script src="js/auto-complete.js"></script>
