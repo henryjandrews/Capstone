1b94493a65b26f94f5677e0313836c09ec6b6c4f
mosleylm <mosleylm@miamioh.edu>
2016-12-06 18:08:26 -0500

    go format string to work

--- Capstone-master/capstoneWebsite/php/database.php
+++ Capstone-master/capstoneWebsite/php/database.php
@@ -43,7 +43,7 @@ function searchForKeyword($keyword) {
 
 // This is the 'askQuestion' function that will add a new question to the DB
 function askQuestion($keyword) {
-    //$keyword = formatString($keyword);
+    $keyword = formatString($keyword);
     $db = getDbConnection();
     $stmt = $db->prepare("INSERT INTO `mydb`.`Question` (`Question_text`) VALUES (?)");
 
@@ -84,7 +84,7 @@ function getAnswers($qID) {
 }
 
 function postAnswer($qID, $aText) {
-    //$aText = formatString($aText);
+    $aText = formatString($aText);
     $db = getDbConnection();
     $stmt = $db->prepare("START TRANSACTION; INSERT INTO `mydb`.`Answer` (`Answer_Text`, `time`)
             VALUES (?, now()); 
